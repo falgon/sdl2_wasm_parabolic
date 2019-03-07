8378eaaf85288e3039c1042b4561292addc2d407
falgon <falgon53@gmail.com>
2019-03-08 00:40:54 +0900

    remove extra code

--- src/scene/drawer.hpp
+++ src/scene/drawer.hpp
@@ -160,10 +160,8 @@ private:
 #else
             wait_event
 #endif
-            ([/*&clicked,*/ this](const SDL_Event& e) -> basic_drawer::loop_result {
-            /*if (e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_LEFT && e.button.state == SDL_PRESSED) {
-                clicked = true;
-            } else*/ if (e.type == SDL_MOUSEBUTTONUP && e.button.button == SDL_BUTTON_LEFT && e.button.state == SDL_RELEASED/* && clicked*/) {
+            ([this](const SDL_Event& e) -> basic_drawer::loop_result {
+             if (e.type == SDL_MOUSEBUTTONUP && e.button.button == SDL_BUTTON_LEFT && e.button.state == SDL_RELEASED) {
                 tf_.reset_static_texture();
 #ifdef __EMSCRIPTEN__
                 tl_.reset_static_texture();
